283adb1a27fc7551c40fca4b19a0036943cc9f75
José Gama <ze.martimgama@gmail.com>
2021-02-07 23:22:32 +0000

    Atualização de usuário com PUT

--- src/main/java/com/zegama/workshopmongo/resources/UserResource.java
+++ src/main/java/com/zegama/workshopmongo/resources/UserResource.java
@@ -44,14 +44,21 @@ public class UserResource {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build();
-		
+		return ResponseEntity.created(uri).build();		
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();	
 	}
 
 }
