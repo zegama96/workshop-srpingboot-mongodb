4095a12bb9cf8935e20b5aa97472719694eca705
José Gama <ze.martimgama@gmail.com>
2021-02-07 22:49:07 +0000

    Obtendo um usuário por id

--- src/main/java/com/zegama/workshopmongo/resources/UserResource.java
+++ src/main/java/com/zegama/workshopmongo/resources/UserResource.java
@@ -1,10 +1,12 @@
 package com.zegama.workshopmongo.resources;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +26,14 @@ public class UserResource {
 	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList()); 
-		return ResponseEntity.ok().body(listDto);
+		return ResponseEntity.ok().body(listDto);		
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		Optional<User> obj = Optional.ofNullable(service.findById(id));
+		return ResponseEntity.ok().body(new UserDTO(obj.get()));
+		
 	}
 
 }
