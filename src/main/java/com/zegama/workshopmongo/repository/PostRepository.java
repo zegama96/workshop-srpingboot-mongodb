a400a0334d167064f6f9e27e11fd17835eea228e
José Gama <ze.martimgama@gmail.com>
2021-02-08 18:21:42 +0000

    Consulta simples com query methods

--- src/main/java/com/zegama/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/zegama/workshopmongo/repository/PostRepository.java
@@ -1,12 +1,15 @@
 package com.zegama.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.zegama.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{	
+public interface PostRepository extends MongoRepository<Post, String>{
 	
-
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
--- src/main/java/com/zegama/workshopmongo/services/PostService.java
+++ src/main/java/com/zegama/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.zegama.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,7 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+	public List<Post> findByTiltle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
